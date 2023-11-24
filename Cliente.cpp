238a107521984cc2619db34342120d9e18735d88
franv <fran.vicente03@gmail.com>
2023-11-24 15:08:16 -0300

    eliminio comentarios de cliente.cpp

--- Cliente.cpp
+++ Cliente.cpp
@@ -9,41 +9,21 @@ Cliente::Cliente(int Dni, string Nombre, string Mail, string Telefono): dni(Dni)
 }
 
 void Cliente::EliminarInmuebleInteres(Inmueble* inmueble) {
-    // auto it = std::find(inmueblesInteres.begin(), inmueblesInteres.end(), inmueble);
-
-    // if (it != inmueblesInteres.end()) {
-    //     delete *it;
-    //     inmueblesInteres.erase(it);
-    // }
-
-
-
-    // for (Inmueble* i:inmueblesInteres)
-    // {
-    //     if (i == inmueble)
-    //     {
-    //         delete(i);
-
-    //     }
-        
-    // }
-    
+    auto it = std::find(inmueblesInteres.begin(), inmueblesInteres.end(), inmueble);
+    if (it != inmueblesInteres.end()) {
+        delete *it;
+        inmueblesInteres.erase(it);
+    }
 }
 
 void Cliente::AgregarInmuebleInteres(Inmueble* inmueble) {
-    // inmueblesInteres.push_back(inmueble);
-
-
-    // auto it = std::find(inmueblesInteres.begin(), inmueblesInteres.end(), inmueble);
-
-    // // Verificar si el inmueble está en la lista
-    // if (it != inmueblesInteres.end()) {
-    //     std::cout << "El inmueble ya está en la lista." << std::endl;
-    // } else {
-    //     // El inmueble no está en la lista, así que agrégalo
-    //     inmueblesInteres.push_back(inmueble);
-    //     std::cout << "El inmueble ha sido agregado a la lista." << std::endl;
-    // }
+    auto it = std::find(inmueblesInteres.begin(), inmueblesInteres.end(), inmueble);
+    if (it != inmueblesInteres.end()) {
+        std::cout << "El inmueble ya está en la lista." << std::endl;
+    } else {
+        inmueblesInteres.push_back(inmueble);
+        std::cout << "El inmueble ha sido agregado a la lista." << std::endl;
+    }
 }
 
 string Cliente::GetMail() {
